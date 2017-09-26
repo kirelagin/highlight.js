2c6645cc95dbfec3083125c33ab428a03e2a1d4e
Kirill Elagin <kirelagin@gmail.com>
2017-09-26 22:15:38 +0300

    arend: Drastically improve highlighting

--- src/languages/arend.js
+++ src/languages/arend.js
@@ -6,11 +6,15 @@ Category: functional
 */
 
 function(hljs) {
-  var KEYWORD = {
-    className: 'keyword',
-    begin: /\\[^\s]+/,
-    relevance: 0
-  };
+  var RE_LEXEMES = /\\?[a-zA-Z0-9-]+|[^\w\s]+/;
+
+  var PARENS = {
+    variants: [
+      {begin: /\(/, end: /\)/},
+      {begin: /\{/, end: /\}/},
+    ],
+    contains: ['self'],
+  }
 
   var COMMENT = {
     variants: [
@@ -25,10 +29,87 @@ function(hljs) {
     ]
   };
 
+  var DEF_TELE = function(kw) {
+    return {
+      className: 'keyword',
+      begin: '\\\\' + kw, end: /\s+/,
+      excludeEnd: true,
+      starts:
+      {
+        className: 'title',
+        begin: /[\S]+/, end: /[\s]+/,
+        excludeEnd: true,
+        starts: {
+          className: 'test',
+          end: /\s*(=>|\|)/,
+          returnEnd: true,
+          contains:
+          [
+            {
+              className: 'params',
+              variants: [
+                {begin: /\(/, end: /\)/},
+                {begin: /\{/, end: /\}/},
+              ],
+              lexemes: RE_LEXEMES,
+              contains: [PARENS],
+            },
+            {
+              className: 'type',
+              begin: /:\s*/,
+              excludeBegin: true,
+              endsWithParent: true,
+              excludeEnd: true,
+            }
+          ]
+        }
+      }
+    };
+  };
+
+  var DEF_NAMED = function(kw) {
+    return {
+      className: 'keyword',
+      begin: '\\\\' + kw, end: /\s+/,
+      excludeEnd: true,
+      starts: {
+        className: 'title',
+        begin: /[\S]+/, end: /[\s]+/,
+        excludeEnd: true,
+      }
+    };
+  };
+
   return {
     aliases: ['vclang'],
-    contains: [
-      KEYWORD,
+    lexemes: RE_LEXEMES,
+    keywords: {
+      keyword:
+        '=> -> : | ' +
+        '\\lp \\lh \\levels \\suc \\max ' +
+        //'\\Pi \\Sigma \\lam \\new ' +
+        '\\let \\in \\case ' +
+        '\\elim \\with ' +
+        '\\infix \\infixl \\infixr ' +
+        '\\default \\truncated ' +
+        '\\open \\export \\hiding ' +
+        '\\where',
+     built_in:
+        'Nat zero suc ' +
+        'I left right ' +
+        'Path path = @ coe iso ' +
+        'TrP inP truncP ' +
+        'TrS inS truncS ',
+      symbol: '{?}'
+    },
+    contains:
+    [
+      DEF_TELE('function'),
+      DEF_TELE('data'),
+      DEF_TELE('instance'),
+      DEF_NAMED('class'),
+      DEF_NAMED('view'),
+
       hljs.NUMBER_MODE,
 
       COMMENT
